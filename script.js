c7b6a7cf02475126351a500c59d0a991c40f02ad
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2024-06-22 19:58:31 -0300

    ccode documentation

---
+++ script.js
@@ -0,0 +1,40 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', () => {
+        const body = accordion.querySelector('.accordion-body');
+        body.classList.toggle('active');
+    })
+})
+
+const linkBlocos = document.querySelector('.link-blocos');
+const linkEditor = document.querySelector('.link-editor');
+
+linkBlocos.addEventListener('click', (event) => {
+    document.querySelector('.hamburger').click();
+    if(!document.querySelector('.accordion-blocos').classList.contains('active')){
+        document.querySelector('#mostrar-blocos').click();
+    }
+})
+linkEditor.addEventListener('click', (event) => {
+    document.querySelector('.hamburger').click();
+    if(!document.querySelector('.accordion-editor').classList.contains('active')){
+        if(document.querySelector('.accordion-blocos').classList.contains('active')){
+            document.querySelector('.accordion-blocos').classList.remove('active');
+            console.log('por favor aparece ifo');
+        } else {
+            console.log('por favor aparece elese');
+        }
+        document.querySelector('#mostrar-editor').click();
+    } else {
+        if(document.querySelector('.accordion-blocos').classList.contains('active')){
+            document.querySelector('.accordion-blocos').classList.remove('active');
+        }
+    }
+})
+
